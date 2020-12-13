ad3acaf2e94fe4e8e446b132c17a46cc9e4d6bf7
pumaliga <a.kolesnikov040498@gmail.com>
2020-12-14 01:39:44 +0200

    new save in 3_1_1

--- hw_4/umnogenia.py
+++ hw_4/umnogenia.py
@@ -1,4 +1,7 @@
-for i in range(1, 11):
-    for j in range(1, 11):
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(i * j, end = ' \t | ')
+#     print('\n')
+for i in range(1, 11): for j in range(1, 11):
         print(i * j, end = ' \t | ')
     print('\n')
